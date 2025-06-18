48cc42f64a3e3f33f856c382e4ee8b19c56cd44f
Douglas Marcelo Monquero <douglas.monquero@gmail.com>
2025-06-18 10:59:31 -0300

    ajustes finos

--- script.js
+++ script.js
@@ -5,3 +5,33 @@ menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("active");
   menuIcon.classList.toggle("bx-x"); // troca ícone de menu para X
 });
+
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.addEventListener('click', () => {
+    navbar.classList.remove('active');
+    menuIcon.classList.remove('bx-x');
+  });
+});
+
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll(".navbar a");
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 150;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${sectionId}`) {
+          link.classList.add("active");
+        }
+      });
+    }
+  });
+});
+
