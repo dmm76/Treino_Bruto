cf640cbbe1d9a3e9adf2b2449729afce8f7c3f3e
Douglas Marcelo Monquero <douglas.monquero@gmail.com>
2025-06-29 16:37:21 -0300

    Ajustes nos links do footer e responsividade navbar

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const menuIcon = document.querySelector("#menu-icon");
 const navbar = document.querySelector(".navbar");
 
+//Abre/fecha o menu hamburguer com clique no ícone
 menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("active");
   menuIcon.classList.toggle("bx-x"); // troca ícone de menu para X
 });
-
+//Fecha o menu ao clicar em qualquer link do menu
 document.querySelectorAll('.navbar a').forEach(link => {
   link.addEventListener('click', () => {
     navbar.classList.remove('active');
     menuIcon.classList.remove('bx-x');
   });
 });
-
+//Destaca o link do menu referente à seção visível na tela (scrollspy)
 const sections = document.querySelectorAll("section[id]");
 const navLinks = document.querySelectorAll(".navbar a");
 
